262f836552ba9e0f25143788f13add45f5b01f0b
AJone099 <105975252+AJone099@users.noreply.github.com>
2024-07-20 18:14:37 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const socket = io("/");
+
+$(function () {
+    $("#send").click(function () {
+        if($("#chat_message").val().length !== 0) {
+            console.log(user);
+            socket.emit("message", $("#chat_message").val());
+            $("chat_message").val("");
+        }
+    })
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+})
